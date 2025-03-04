534c25bf21c85f1003a890c14699b84aa4f309ba
Angelo Cesaro <acesaro@confluent.io>
2025-03-04 19:17:46 +0100

    adding first files

--- kafkaacls/kafkaacls1.sh
+++
@@ -1,8 +0,0 @@
-kafka-acls:
-  action: "--add"
-  allow-principal: "User:team1"
-  operations:
-    - "WRITE"
-    - "DESCRIBE"
-  transactional-id: "team1-streams-app1"
-  resource-pattern-type: "prefixed"
